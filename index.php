bd458cf031066b5074d08419ad7401f32b8c0c1a
milan <milan@intelithub.com>
2016-05-23 18:45:24 +0530

    1.First Time

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'libs/view.php';
 require_once 'libs/controller.php';
 require_once 'libs/bootstrap.php';
 require_once 'libs/functions.php'; 
-echo 'Which changes are here why why?'; 
+echo 'Which changes are here why why?'; echo "I am changing here"; 
 
 
 
