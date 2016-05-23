aeb8331a4662d0bd0a78bf467bf3a9c474517a20
Manish-Goswami <manish18591@gmail.com>
2016-05-23 18:33:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once 'libs/view.php';
 require_once 'libs/controller.php';
 require_once 'libs/bootstrap.php';
-require_once 'libs/functions.php';
+require_once 'libs/functions.php'; 
+This is my code.
 
 
 
