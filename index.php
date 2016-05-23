4af8d53a925f982f0fd81f267f37fa2e1bdd4e12
Manish-Goswami <manish18591@gmail.com>
2016-05-23 18:39:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'libs/view.php';
 require_once 'libs/controller.php';
 require_once 'libs/bootstrap.php';
 require_once 'libs/functions.php'; 
-echo 'Same line changes'; 
+echo 'Which changes are here'; 
 
 
 
